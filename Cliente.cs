e56dcf2575898423505a5d8adfd61aa0445fc6de
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-08-11 04:37:39 +0200

    Add files via upload
    I SERIE.... Empresa

---
+++ Cliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes
+{
+    class Cliente
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Nit { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string NomVendedor { get; set; }
+        public string TipoCliente { get; set; }
+        public double TotalComprasClientes { get; set; }
+
+        public Cliente(int codigo,string nombre,string apellidos,string nit,string direccion,string telefono,string vendedor,double totalcompras)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Nit = nit;
+            this.Direccion = direccion;
+            this.Telefono = telefono;
+            this.NomVendedor = vendedor;
+            this.TotalComprasClientes = totalcompras;
+            if (totalcompras<=1500)
+            {
+                this.TipoCliente = "Cliente Normal";
+                
+            }
+            if (totalcompras>1501 && totalcompras<5000)
+            {
+                if (this .TipoCliente =="Cliente Normal")
+                {
+                    this.TipoCliente = "Cliente Normal";
+                }
+                else
+                {
+                    this.TipoCliente = "Cliente Especial";
+                }
+               
+                
+            }
+            if (totalcompras >5001)
+            {
+                this.TipoCliente = "Cliente Mayorista";
+                
+            }
+        }
+
+        //-------------Metodo----------------
+        public void Imprimir()
+        {
+            Console.WriteLine("Codigo: " + Codigo + " Nombre: " + Nombre + " Apellidos: " + Apellidos +" Nit: " + Nit + "\nDireccion: " + Direccion + " Telefono: " + Telefono + " Vendedor: " + NomVendedor +"\nTotal Compras:"+TotalComprasClientes+ " Tipo Cliente: " + TipoCliente);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============================Cliente 1==============================");
+            Cliente Cliente1 = new Cliente(1, "Manuel Enrique", "Hernandez Ceballos", "4547878545", "Mazatenango", "45457852", "Luis Paz", 1500);
+            Cliente1.Imprimir();
+            Console.WriteLine("==============================Cliente 2==============================");
+            Cliente Cliente2 = new Cliente(3, "Jose Manuel", "Valle Garcia", "4547878545", "Mazatenango", "45457852", "Luis Paz", 1600);
+            Cliente2.Imprimir();
+            Console.WriteLine("==============================Cliente 3==============================");
+            Cliente Cliente3 = new Cliente(4, "Luis Rodrigo", "Hernandez Ceballos", "4547878545", "Mazatenango", "45457852", "Luis Paz", 6000);
+            Cliente3.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
