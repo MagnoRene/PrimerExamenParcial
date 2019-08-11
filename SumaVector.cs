bbb801dbdf445ad94bb1c06b6787b278054eea59
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-08-11 04:43:38 +0200

    II Serie .....
    II Serie .....

---
+++ SumaVector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvFi
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            sumvector sum = new sumvector(new List<int> { 1, 2, 3 });
+            sumvector su = new sumvector(new List<int> { 4,5, 6 });
+            sumvector mo1 = su.Suma(sum);
+            Console.WriteLine("Suma");
+            foreach (var item in mo1.Componentes)
+            {
+                Console.WriteLine(item);
+            }
+
+            sumvector mo2 = su.Resta(sum);
+            Console.WriteLine("Resta");
+            foreach (var item in mo2.Componentes)
+            {
+                Console.WriteLine(item);
+            }
+
+            sumvector mo3 = su.Multiplicar(sum);
+            Console.WriteLine("Multiplicacion");
+            foreach (var item in mo3.Componentes)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ sumvector.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvFi
+{
+    class sumvector
+    {
+        #region Parte1 
+
+        // explicar por constructor
+        private List<int> _Componentes;
+
+        //constructor
+        public sumvector(List<int> Componentes)
+        {
+            _Componentes = Componentes;
+
+        }
+
+        public sumvector()
+        {
+        }
+        #endregion
+
+        public List<int> Componentes
+        {
+            get
+            {
+                return _Componentes;
+            }
+        }
+
+        public int this[int i]
+        {
+            get { return _Componentes[i]; }
+            set { _Componentes[i] = value; }
+        }
+        public int Dimension { get { return _Componentes.Count; } }
+
+
+        public static sumvector operator +(sumvector v1, sumvector v2)
+        {
+            return v1.Suma(v2);
+        }
+        public static sumvector operator -(sumvector v1, sumvector v2)
+        {
+            return v1.Resta(v2);
+        }
+        public static sumvector operator *(sumvector v1, sumvector v2)
+        {
+            return v1.Multiplicar(v2);
+        }
+
+
+        public double PE(sumvector v2)
+        {
+            if (Dimension != v2.Dimension)
+            {
+                throw new ApplicationException("las dimensiones no son iguales");
+            }
+
+            //creamos una lista  llamada resultado            
+            double resultado = 0;
+
+            for (int i = 0; i < Dimension; i++)
+            {
+                //resultado.Add(this[i] + v2[i]);
+                resultado += (this[i]) + (v2[i]);
+            }
+
+            return resultado;
+        }
+
+        // suma de vectores =  a la suma  de las componentes correspondientes de ambos vectos
+        public sumvector Suma(sumvector v2)
+        {
+            if (Dimension != v2.Dimension)
+            {
+                throw new ApplicationException("las dimensiones no son iguales");
+            }
+
+            //creamos una lista  llamada resultado
+
+            List<int> resultado = new List<int>();
+
+            for (int i = 0; i < Dimension; i++)
+            {
+                resultado.Add(this[i] + v2[i]);
+            }
+
+            return new sumvector(resultado);
+        }
+
+
+        public sumvector Resta(sumvector v2)
+        {
+            if (Dimension != v2.Dimension)
+            {
+                throw new ApplicationException("las dimensiones no son iguales");
+            }
+
+            //creamos una lista  llamada resultado
+
+            List<int> resultado = new List<int>();
+
+            for (int i = 0; i < Dimension; i++)
+            {
+                resultado.Add(this[i] - v2[i]);
+            }
+
+            return new sumvector(resultado);
+        }
+
+
+        public sumvector Multiplicar(sumvector v2)
+        {
+            if (Dimension != v2.Dimension)
+            {
+                throw new ApplicationException("las dimensiones no son iguales");
+            }
+
+            //creamos una lista  llamada resultado
+
+            List<int> resultado = new List<int>();
+
+            for (int i = 0; i < Dimension; i++)
+            {
+                resultado.Add(this[i] * v2[i]);
+            }
+
+            return new sumvector(resultado);
+        }
+    }
+}
